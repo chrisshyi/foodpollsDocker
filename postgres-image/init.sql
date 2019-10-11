b4b58a160649992cb93aa1f8427f5435b9965510
Chris Shyi <chris.shyi@mail.utoronto.ca>
2019-10-11 13:10:10 +0800

    Problem with static files
    Experiencing problems serving static files.

--- postgres-image/init.sql
+++ postgres-image/init.sql
@@ -3,5 +3,5 @@ CREATE USER django WITH PASSWORD 'django';
 ALTER ROLE django SET client_encoding TO 'utf8';
 ALTER ROLE django SET default_transaction_isolation TO 'read committed';
 ALTER ROLE django SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE appe TO django; 
+GRANT ALL PRIVILEGES ON DATABASE foodpolls TO django; 
 ALTER ROLE django CREATEDB;
