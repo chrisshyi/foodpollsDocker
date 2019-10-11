2ab30452eee25fc834bae4ddb089a71e384b8401
Chris Shyi <chris.shyi@mail.utoronto.ca>
2019-10-11 11:18:28 +0800

    Initial commit

---
+++ postgres-image/init.sql
@@ -0,0 +1,7 @@
+CREATE USER django WITH PASSWORD 'django';
+
+ALTER ROLE django SET client_encoding TO 'utf8';
+ALTER ROLE django SET default_transaction_isolation TO 'read committed';
+ALTER ROLE django SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE appe TO django; 
+ALTER ROLE django CREATEDB;
